a3386518e174183e8d94497013d759a9722a4599
Marina <dgoun16@mail.ru>
2022-10-01 22:43:10 +0300

    Добавили программу

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿void GetArray()
+      {  
+      Console.WriteLine("Введите количество элементов в массиве N:");
+      int N = int.Parse(Console.ReadLine());
+      
+      string[] arr = new string[N];
+      
+      Console.WriteLine("Введите элементы массива:");
+
+      for (int i = 0; i < N; i++)
+      {
+        arr[i] = Console.ReadLine();
+      }
+           
+      string [] arr2 = new string[N];      
+      
+      int k = 0;
+      
+      for (int i = 0; i < N; i++)
+      
+        {
+        int length = arr[i].Length;    
+               
+        if (length <= 3)
+          {     
+            arr2[k] = arr[i];
+            k++;
+          }
+          
+        }
+
+      Console.Write($"['{string.Join("', '", arr)}']");
+      Console.WriteLine();
+      
+      Array.Resize(ref arr2, k);  
+      Console.Write($"['{string.Join("', '", arr2)}']");
+      Console.WriteLine();
+      }
+
+      GetArray();
+      
